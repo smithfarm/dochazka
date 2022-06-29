954f1597dff5dc5505cf8f8ceaeee3629ad04731
Nathan Cutler <ncutler@suse.com>
2022-06-29 11:47:40 +0200

    bootstrap.sh: really add the new file
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+sudo zypper --gpg-auto-import-keys --non-interactive refresh
+sudo zypper --non-interactive install \
+    perl-File-Next \
+    perl-Log-Any \
+    perl-Params-Validate \
+    perl-File-ShareDir-Install \
+    perl-Test-Deep \
+    perl-Test-Output \
+    perl-Test-Warnings \
+    perl-Perl-Version \
+    perl-CPAN-Uploader \
+    cpanspec
